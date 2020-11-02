61e67a56dcd0553d026d58b3029717783d8342c4
rajdeepsr7 <rajdeepsinghranawatninja@gmail.com>
2020-11-03 02:58:51 +0530

    .

--- js/chat.js
+++ js/chat.js
@@ -8,6 +8,8 @@
             let newChatWindow = getNewChatWindow(userId);
 
             addChatBoxDelete($('.delete-chat-box',newChatWindow)[0]);
+            addMinimizeBoxButton($('.minimize-chat-box',newChatWindow)[0]);
+            sendMessage($('.chat-submit-button',newChatWindow));
 
             $('#chat-box').append(newChatWindow);
         })
@@ -28,9 +30,29 @@
 
             <span class="chat-details flex">
 
-                User ${userId}  <button class="delete-chat-box" id="${userId}">X</button>  <button class="delete-chat-box" id="${userId}">-</button>
+                User ${userId}  <button class="delete-chat-box" id="${userId}">X</button>  <button class="minimize-chat-box" id="${userId}" value="1">-</button>
 
             </span>
+            
+
+            <div class="message-box flex" id="message-box-${userId}">
+
+                <div class="messages flex" id="messages-${userId}">
+                    <span class="other-message"><span>Hi! How u doing?</span></span>
+                </div>
+
+
+                <form action="">
+                    <input type="text" class="chat-content" id="message-content-${userId}" placeholder="Message...">
+                    <input type="submit" class="chat-submit-button" id="${userId}">
+                </form>
+                
+
+                
+            </div>
+
+            
+            
 
         <div>
         
@@ -46,5 +68,43 @@
 
             $(chatBoxId).remove();
         })
+    }
+
+    function addMinimizeBoxButton(minimizeChatBoxButton){
+        $(minimizeChatBoxButton).on('click',function(e){
+            e.preventDefault();
+
+            let messageBoxId = '#message-box-'+$(minimizeChatBoxButton).attr('id');
+
+            if($(messageBoxId).attr('value')=='1'){
+                $(messageBoxId).css('height','0px');
+                $(messageBoxId).attr('value','0');
+            }else{
+                $(messageBoxId).css('height','300px');
+                $(messageBoxId).attr('value','1');
+            }
+
+        })
+    }
+
+    function sendMessage(sendButton){
+        $(sendButton).on('click',function(e){
+            e.preventDefault();
+
+            let contentId = '#message-content-'+$(sendButton).attr('id');
+            let content = $(contentId).val();
+
+            if(content == ''){
+                return;
+            }
+
+            let newMessage = $(`
+
+             <span class="self-message"><span>${content}</span></span>
+            `);
+
+            $('#messages-'+$(sendButton).attr('id')).append(newMessage);
+
+        });
     }
 }
